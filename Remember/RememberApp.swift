c96c8321b810ac42987844a105f3018e9e69b138
latif <latifkhanna0899@gmail>
2026-03-18 12:58:06 +0500

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Latif on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
